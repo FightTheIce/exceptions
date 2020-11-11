55d1fff62113d310f404a9dcd7fc7b270f7ca48c
William Knauss <william.knauss@kingdom.com>
2020-11-10 21:25:43 -0500

    some updates

--- src/ComponentExceptionTrait.php
+++ src/ComponentExceptionTrait.php
@@ -4,15 +4,40 @@ namespace FightTheIce\Exceptions;
 
 trait ComponentExceptionTrait
 {
-    protected $componentName = 'UNKNOWN';
+    /**
+     * componentName
+     *
+     * @var string
+     */
+    protected string $componentName = 'UNKNOWN';
 
-    public function getComponentName()
+    public function getComponentName() : string
     {
         return $this->componentName;
     }
 
-    public function setComponentName($component)
+    /**
+     * setComponentName
+     *
+     * @param   string  $component  
+     *
+     * @return  mixed
+     */
+    public function setComponentName(string $component)
     {
         $this->componentName = $component;
+        
+        return $this;
+    }
+
+    /**
+     * raise
+     * Throw the exception
+     * @note - Don't use this... it will screw up backtrace
+     *
+     * @return  void
+     */
+    public function raise() {
+        throw $this;
     }
 }
--- src/ExceptionsInterface.php
+++ src/ExceptionsInterface.php
@@ -4,6 +4,14 @@ namespace FightTheIce\Exceptions;
 
 interface ExceptionsInterface
 {
-    public function getComponentName();
-    public function setComponentName($component);
+    public function getComponentName(): string;
+
+    /**
+     * setComponentName
+     *
+     * @param   string  $component  
+     *
+     * @return  mixed
+     */
+    public function setComponentName(string $component);
 }
